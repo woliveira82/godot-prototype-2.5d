ca7b19cc24b24c7b794302f74cd718da860eb8db
Weiner Oliveira <woliveira82@gmail.com>
2026-05-28 21:07:49 +0100

    refac: enemy knockback effect

--- enemy.gd
+++ enemy.gd
@@ -19,6 +19,7 @@ const FaceDirection = {
 @export var gravity := 12.0
 @export var max_hp := 5
 @export var damage := 1
+@export var knockback_force := 8.0
 
 var hp := max_hp
 
@@ -28,6 +29,7 @@ var can_damage := true
 var facing: String = FaceDirection.DOWN
 var stun_time := 0.0
 var knockback_velocity := Vector3.ZERO
+var knockback := false
 
 @onready var sprite := $VisualPivot/AnimatedSprite3D
 @onready var visual := $VisualPivot
@@ -44,20 +46,12 @@ func _physics_process(delta):
 	if current_state == State.DEAD:
 		return
 
-	if current_state == State.HURT:
-		move_and_slide()
-		return
-
 	if current_state == State.STUN:
 		stun_time -= delta
 		velocity.x += knockback_velocity.x
 		velocity.z += knockback_velocity.z
 		knockback_velocity = knockback_velocity.lerp(Vector3.ZERO, delta * 8.0)
 		move_and_slide()
-		if stun_time <= 0.0:
-			current_state = State.CHASE
-
-		return
 
 	_update_state()
 	_handle_movement(delta)
@@ -66,7 +60,8 @@ func _physics_process(delta):
 
 func _update_state():
 	if current_state == State.STUN:
-		return
+		if stun_time > 0.0:
+			return
 
 	if current_state == State.HURT:
 		return
@@ -84,6 +79,11 @@ func _update_state():
 func _handle_movement(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+
+	if knockback:
+		velocity.x += knockback_velocity.x * knockback_force
+		velocity.z += knockback_velocity.z * knockback_force
+		#knockback_velocity = knockback_velocity.lerp(Vector3.ZERO, delta * 8.0)
 
 	if current_state == State.CHASE:
 		var dir = (player.global_position - global_position).normalized()
@@ -121,8 +121,8 @@ func _update_animation():
 		State.CHASE:
 			anim = "run_" + facing
 
-		State.HURT:
-			anim = "idle_down"
+		State.STUN:
+			anim = "idle_" + facing
 
 		State.DEAD:
 			anim = "idle_down"
@@ -136,11 +136,11 @@ func take_damage(amount: int):
 
 	hp -= amount
 	_hit_feedback()
-	var hit_dir = (global_position - player.global_position).normalized()
-	knockback_velocity = hit_dir 
+	knockback_velocity = (global_position - player.global_position).normalized()
 
 	current_state = State.STUN
 	stun_time = 1.0
+	knockback = true
 
 	if hp <= 0:
 		current_state = State.DEAD
